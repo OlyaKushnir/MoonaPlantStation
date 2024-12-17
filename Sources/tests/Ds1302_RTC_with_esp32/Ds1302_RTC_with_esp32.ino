a4e46065e83cae8febac8c420005ba9df35e9ebd
olya <olya.kushnir@gmail.com>
2024-12-17 14:03:55 +0200

    timer

--- Sources/tests/Ds1302_RTC_with_esp32/Ds1302_RTC_with_esp32.ino
+++ Sources/tests/Ds1302_RTC_with_esp32/Ds1302_RTC_with_esp32.ino
@@ -11,8 +11,15 @@ void setup() {
     }
 }
 
+DateTime start;
 void loop() {
   DateTime now = rtc.getTime();
+
+  static char buf[20];
+  TimeDelta firstEventTimeStamp(7, 0, 0, 0);
+  Serial.print("7 days in the furture:");
+  Serial.println((now + firstEventTimeStamp).tostr(buf));
+
   rtc.printTimeInfo();
   delay(3000);
 }
